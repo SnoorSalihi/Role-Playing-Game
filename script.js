45cb98fb0490a2f5b78aec5129dd644a7d1e1c7d
SnoorSalihi <eng.sinoor@gmail.com>
2024-05-04 23:56:40 -0400

    updated

--- script.js
+++ script.js
@@ -20,6 +20,15 @@ const monsterHealthText = document.querySelector("#monsterHealth");
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
+function goTown() {
+  button1.innerText = "Go to store";
+  button2.innerText = "Go to cave";
+  button3.innerText = "Fight dragon";
+  button1.onclick = goStore;
+  button2.onclick = goCave;
+  button3.onclick = fightDragon;
+  text.innerText = "You are in the town square. You see a sign that says Store.";
+}
 function goStore() {
     button1.innerText = "Buy 10 health (10 gold)";
     button2.innerText = "Buy weapon (30 gold)";
@@ -27,6 +36,7 @@ function goStore() {
     button1.onclick = buyHealth;
     button2.onclick = buyWeapon;
     button3.onclick = goTown;
+    text.innerText = "You enter the store.";
   }
 function goCave() {
     console.log("Going to cave.");
@@ -34,4 +44,10 @@ function goCave() {
   
 function fightDragon() {
     console.log("Fighting dragon.");
+  }
+function buyHealth(){
+
+  }
+function buyWeapon(){
+    
   }
